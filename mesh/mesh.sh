19394e622895ff9fa14a9d880544fd932df8b62c
sofiashalotenko <svs3jm@virginia.edu>
2019-11-05 11:00:19 -0500

    work thus far

--- mesh/mesh.sh
+++ mesh/mesh.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
+echo "start edge"
 /opt/mesh/edge.sh
 
 echo "done edge"
 
 sleep 10s
 
+echo "start control-api"
 /opt/mesh/control-api.sh
 
 echo "done control-api"
 
-sleep 10s
+# sleep 10s
 
-/opt/mesh/catalog.sh
+# echo "start catalog"
+# /opt/mesh/catalog.sh
 
-echo "done catalog"
+# echo "done catalog"
 
-sleep 10s
+# sleep 10s
 
-/opt/mesh/dashboard.sh
+# echo "start dashboard"
+# /opt/mesh/dashboard.sh
 
-echo "done dashboard"
+# echo "done dashboard"
+
+# echo "start data"
+# /opt/mesh/data.sh
+
+# echo "done data"
 
 echo "list clusters"
 
 /opt/mesh/greymatter list cluster
-
-echo "list domains"
-
-/opt/mesh/greymatter list domain
-
-echo "list listeners"
-
-/opt/mesh/greymatter list listener
 
 /usr/sbin/sshd -D
